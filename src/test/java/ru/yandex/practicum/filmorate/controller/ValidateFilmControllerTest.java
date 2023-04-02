ca9b5f1484c90538a3309a7307bcea2b3c8df7ce
Konstantin <ya-konstant@yandex.ru>
2023-04-02 14:53:12 +0300

    на проверку по тз 10

--- src/test/java/ru/yandex/practicum/filmorate/controller/ValidateFilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/ValidateFilmControllerTest.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.Storage.film.InMemoryFilmStorage;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
@@ -35,65 +33,5 @@ class ValidateFilmControllerTest {
         inMemoryFilmStorage.addFilm(film);
 
         assertEquals(1, inMemoryFilmStorage.getFilms().size());
-    }
-
-    @Test
-    void failDescription() {
-        filmController = new FilmController();
-        Film film = Film.builder()
-                .id(1)
-                .name("name")
-                .description("Пятеро друзей ( комик-группа «Шарло»), приезжают в город Бризуль. Здесь они хотят " +
-                        "разыскать господина Огюста Куглова, который задолжал им деньги, а именно 20 миллионов. о " +
-                        "Куглов, который за время «своего отсутствия», стал кандидатом Коломбани.")
-                .releaseDate(LocalDate.of(2022, Month.JANUARY, 1))
-                .duration(100)
-                .build();
-        System.out.println(film);
-
-//        ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
-//            filmService.validate(film);
-//        });
-//
-//        Assertions.assertEquals("Превышено максимальное количество символов: "
-//                + FilmService.MAX_NUMBER_OF_CHARACTERS, thrown.getMessage());
-    }
-
-    @Test
-    void failReleaseDate() {
-        filmController = new FilmController();
-        Film film = Film.builder()
-                .id(1)
-                .name("name")
-                .description("description")
-                .releaseDate(LocalDate.of(1895, Month.DECEMBER, 27))
-                .duration(100)
-                .build();
-
-//        ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
-//            filmService.validate(film);
-//        });
-//
-//        Assertions.assertEquals("Дата релиза фильма раньше "
-//                + FilmService.MIN_RELEASE_DATE, thrown.getMessage());
-    }
-
-    @Test
-    void failDuration() {
-        filmController = new FilmController();
-        Film film = Film.builder()
-                .id(1)
-                .name("name")
-                .description("description")
-                .releaseDate(LocalDate.of(2022, Month.JANUARY, 1))
-                .duration(0)
-                .build();
-
-//        ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
-//            filmService.validate(film);
-//        });
-//
-//        Assertions.assertEquals("Продолжительность фильма в минутах должна быть больше: "
-//                + FilmService.MIN_DURATION_FILM, thrown.getMessage());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/ValidateUserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/ValidateUserControllerTest.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.Storage.user.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
@@ -35,41 +33,5 @@ class ValidateUserControllerTest {
         inMemoryUserStorage.addUser(user);
 
         assertEquals(1, inMemoryUserStorage.getUsers().size());
-    }
-
-    @Test
-    void failBirthday() {
-        userController = new UserController();
-        User user = User.builder()
-                .id(1)
-                .email("ya@ya.ru")
-                .login("login")
-                .name("name")
-                .birthday(LocalDate.of(2081, Month.SEPTEMBER, 1))
-                .build();
-
-//        ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
-//            userService.validate(user);
-//        });
-//
-//        Assertions.assertEquals("Дата рождения не может быть в будущем.", thrown.getMessage());
-    }
-
-    @Test
-    void failLogin() {
-        userController = new UserController();
-        User user = User.builder()
-                .id(1)
-                .email("ya@ya.ru")
-                .login("log in")
-                .name("name")
-                .birthday(LocalDate.of(1981, Month.SEPTEMBER, 1))
-                .build();
-
-//        ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
-//            userService.validate(user);
-//        });
-//
-//        Assertions.assertEquals("Логин не может содержать пробелы", thrown.getMessage());
     }
 }
